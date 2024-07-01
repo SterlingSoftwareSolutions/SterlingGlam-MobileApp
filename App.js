98a9b2b891de218dac3bd75feba9d467d0cdec14
Madumi Danukshi <madhunidhanushika@gmail.com>
2024-07-01 14:36:18 +0530

    update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, StackActions} from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './App/Screens/Welcome';
 import Signup from './App/Screens/SignUp';
@@ -11,16 +11,22 @@ import Appoinment from './App/Screens/Appoinment';
 import AppoinmentHistory from './App/Screens/AppoinmentHistory';
 import SubCategories from './App/Screens/SubCategories';
 import { Profiler } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
- 
+ const Stack = createNativeStackNavigator();
  
 export default function App() {
   return (
 
-    <View style={styles.container}>
-     <CustomerProfile/>
-      
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Welcome'>
+        <Stack.Screen name='welcome' component={Welcome}/>
+        <Stack.Screen name='Login' component={Login}/>
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+ 
     
   );
 }
