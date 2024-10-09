1d14dffa5f07f31134dd402df11554dccf66af73
Thafani Nawaz <114370326+Thafani24@users.noreply.github.com>
2024-10-09 11:45:02 +0530

    added AsyncStorage

--- App/auth/useAuth.js
+++ App/auth/useAuth.js
@@ -1,16 +1,27 @@
-import {useContext} from 'react';
+import { useContext } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import AuthContext from "./context";
 import authService from "./authService";
 
 export default useAuth = () => {
-    const {user, setUser} = useContext(AuthContext);
+    const { user, setUser } = useContext(AuthContext);
 
-    const logIn = (authToken, user) => {
+    const logIn = async (authToken, user) => {
         setUser(user);
         console.log('User login:', user);
+
+        // Store user and token in AsyncStorage
+        await AsyncStorage.setItem('authToken', authToken);
+        await AsyncStorage.setItem('user', JSON.stringify(user));
         authService.storeUser(user);
         authService.storeToken(authToken);
     };
 
-  return { logIn };
+    const logOut = async () => {
+        setUser(null);
+        await AsyncStorage.removeItem('authToken');
+        await AsyncStorage.removeItem('user');
+    };
+
+    return { logIn, logOut };
 };
