d139a29d4e927b3dfb1ce8c06655943012fa3125
Thafani Nawaz <114370326+Thafani24@users.noreply.github.com>
2024-10-09 11:39:13 +0530

    cleaned unused lines

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React, {useState} from 'react';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RootNavigator from './App/navigation/RootNavigator';
 import AuthContext from "./App/auth/context";
 import AdminNavigation from "./App/navigation/AdminNavigation";
-import HomeNavigator from "./App/navigation/HomeNavigator";
 import TabNavigator from './App/navigation/TabNavigator';
 
 const Stack = createNativeStackNavigator();
@@ -38,10 +37,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-
-  },
-});
